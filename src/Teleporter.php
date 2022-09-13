9248b56242bcdb69bb8e2160e33ff6a7b9ddb28d
Ken_Cir0909 <65713982+KenCir@users.noreply.github.com>
2022-09-14 01:07:13 +0900

    翻訳 & コマンド廃止

--- src/Teleporter.php
+++ src/Teleporter.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace outiserver\teleporter;
 
+use Ken_Cir\LibFormAPI\FormStack\StackFormManager;
 use outiserver\teleporter\commands\TeleporterCommand;
 use outiserver\teleporter\database\teleporter\TeleporterDataManager;
+use outiserver\teleporter\language\LanguageManager;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\utils\SingletonTrait;
@@ -19,6 +21,10 @@ class Teleporter extends PluginBase
     private DataConnector $dataConnector;
 
     private TeleporterDataManager $teleporterDataManager;
+
+    private LanguageManager $languageManager;
+
+    private StackFormManager $stackFormManager;
 
     protected function onLoad(): void
     {
@@ -37,7 +43,14 @@ class Teleporter extends PluginBase
 
         $this->teleporterDataManager = new TeleporterDataManager($this->dataConnector);
 
-        $this->getServer()->getCommandMap()->register($this->getName(), new TeleporterCommand($this, "teleporter", "テレポーターコマンド", ["tpr"]));
+        $this->languageManager = new LanguageManager("{$this->getFile()}resources/lang");
+
+        $this->stackFormManager = new StackFormManager();
+
+        $this->getServer()->getCommandMap()->registerAll($this->getName(),
+        [
+            new TeleporterCommand($this, "teleporter", "テレポーターコマンド", "/teleporter", [])
+        ]);
     }
 
     /**
@@ -54,5 +67,13 @@ class Teleporter extends PluginBase
     public function getTeleporterDataManager(): TeleporterDataManager
     {
         return $this->teleporterDataManager;
+    }
+
+    /**
+     * @return StackFormManager
+     */
+    public function getStackFormManager(): StackFormManager
+    {
+        return $this->stackFormManager;
     }
 }
---
+++ src/forms/admin/AddTeleporterForm.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace outiserver\teleporter\forms\admin;
+
+use Ken_Cir\LibFormAPI\FormContents\CustomForm\ContentInput;
+use Ken_Cir\LibFormAPI\FormContents\CustomForm\ContentLabel;
+use Ken_Cir\LibFormAPI\Forms\CustomForm;
+use Ken_Cir\LibFormAPI\Utils\FormUtil;
+use outiserver\economycore\Forms\Base\BaseForm;
+use outiserver\teleporter\database\teleporter\TeleporterDataManager;
+use outiserver\teleporter\language\LanguageManager;
+use outiserver\teleporter\Teleporter;
+use pocketmine\player\Player;
+
+class AddTeleporterForm implements BaseForm
+{
+    public const FORM_KEY = "add_teleporter_form";
+
+    public function execute(Player $player): void
+    {
+        $form = new CustomForm(Teleporter::getInstance(),
+            $player,
+            LanguageManager::getInstance()->getLanguage($player->getLocale())->translateString("form.add_teleporter.title"),
+        [
+            new ContentLabel(LanguageManager::getInstance()->getLanguage($player->getLocale())->translateString("form.add_teleporter.label")),
+            new ContentInput(LanguageManager::getInstance()->getLanguage($player->getLocale())->translateString("form.add_teleporter.input"))
+        ],
+        function (Player $player, array $data) {
+            $pos = $player->getPosition();
+            TeleporterDataManager::getInstance()->create($pos->getWorld()->getFolderName(),
+                $data[1],
+                $pos->getFloorX(),
+                $pos->getFloorY(),
+                $pos->getFloorZ());
+
+            Teleporter::getInstance()->getStackFormManager()->deleteStackForm($player->getXuid(), self::FORM_KEY);
+            FormUtil::backForm(Teleporter::getInstance(), [$this, "execute"], [$player], 3);
+        },
+            function (Player $player) {
+                Teleporter::getInstance()->getStackFormManager()->deleteStackForm($player->getXuid(), self::FORM_KEY);
+                Teleporter::getInstance()->getStackFormManager()->getStackFormEnd($player->getXuid())->reSend();
+            });
+
+        Teleporter::getInstance()->getStackFormManager()->addStackForm($player->getXuid(), self::FORM_KEY, $form);
+    }
+}
